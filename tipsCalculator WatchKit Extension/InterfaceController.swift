318d3eef989655ecd3549733828f165c8e5a527a
Kumar Praveen <kumar.praveen@arvindinternet.com>
2021-07-13 16:00:21 +0530

    Tips calculator

--- tipsCalculator WatchKit Extension/InterfaceController.swift
+++ tipsCalculator WatchKit Extension/InterfaceController.swift
@@ -11,6 +11,22 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    @IBOutlet weak var textField: WKInterfaceTextField!
+    @IBOutlet weak var tenPercentLabel: WKInterfaceLabel!
+    @IBOutlet weak var twentyPercentLabel: WKInterfaceLabel!
+    @IBOutlet weak var thirtyPercentLabel: WKInterfaceLabel!
+    
+    @IBAction func textfieldAction(_ value: NSString?) {
+        let stringText = value as String?
+        if let values = stringText{
+            guard let intValue = Int(values) else { return}
+            tenPercentLabel.setText("\(intValue*10 / 100)")
+            twentyPercentLabel.setText("\(intValue*15 / 100)")
+            thirtyPercentLabel.setText("\(intValue*20 / 100)")
+        }
+    }
+    
+    
     override func awake(withContext context: Any?) {
         // Configure interface objects here.
     }
@@ -22,5 +38,7 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
     }
+    
+    
 
 }
